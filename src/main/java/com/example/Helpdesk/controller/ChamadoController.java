ba41553936890eafc043822d6cb299d44d532997
Rodrigo_C <rodrigo.c.santos8@ba.estudante.senai.br>
2026-08-21 20:19:56 -0300

    Criando Chamados (#3)
    Co-authored-by: Rodrigo costa <rodrigocostas147@gmail.com>

---
+++ src/main/java/com/example/Helpdesk/controller/ChamadoController.java
@@ -0,0 +1,33 @@
+package com.example.Helpdesk.controller;
+
+import com.example.Helpdesk.dto.ChamadoRequestDto;
+import com.example.Helpdesk.dto.ChamadoResponseDto;
+import com.example.Helpdesk.services.ChamadoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chamados")
+public class ChamadoController {
+
+    private final ChamadoService chamadoService;
+
+    public ChamadoController(ChamadoService chamadoService) {
+        this.chamadoService = chamadoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoResponseDto> criar(@Valid @RequestBody ChamadoRequestDto dto) {
+        ChamadoResponseDto chamadoCriado = chamadoService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(chamadoCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoResponseDto>> listarTodos() {
+        return ResponseEntity.ok(chamadoService.listarTodos());
+    }
+}
