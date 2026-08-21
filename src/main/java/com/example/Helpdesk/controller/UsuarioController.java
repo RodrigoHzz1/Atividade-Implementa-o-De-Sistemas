43f806cf10ce7dd5509cc5eb132250b9714b79dc
Rodrigo Costa <rodrigocostas147@gmail.com>
2026-08-20 21:20:10 -0300

    Criando o back

---
+++ src/main/java/com/example/Helpdesk/controller/UsuarioController.java
@@ -0,0 +1,49 @@
+package com.example.Helpdesk.controller;
+
+import com.example.Helpdesk.dto.UsuarioResponseDto;
+import com.example.Helpdesk.dto.UsuarioResquestDto;
+import com.example.Helpdesk.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDto> criar(@Valid @RequestBody UsuarioResquestDto dto) {
+        UsuarioResponseDto novoUsuario = usuarioService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> listarTodos() {
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> atualizar(@PathVariable Long id, @Valid @RequestBody UsuarioResquestDto dto) {
+        return ResponseEntity.ok(usuarioService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
